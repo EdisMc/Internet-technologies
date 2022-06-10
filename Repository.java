dec7f9dfeec303cd72b15ba3e59cacd863f9a5de
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-06-10 20:51:52 +0300

    Add files via upload

--- Repository.java
+++ Repository.java
@@ -1,37 +1,61 @@
-package repository;
+package repositories;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import models.BeautyCompetition;
+import xml.XmlWorker;
 
-import models.Student;
-
+@XmlRootElement(name="competition")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Repository {
+	private static String pathToXml = "C:\\Users\\edy_d\\eclipse-workspace\\it2\\src\\main\\webapp\\xml\\beautyStorage.xml";
 	private static Repository instance = null;
-	private static Set<Student> students;
+	
+	@XmlElement(name="competition")
+	private static Set<BeautyCompetition> competitions = new HashSet<BeautyCompetition>();
 	
 	private Repository() {
-		
 	}
 	
-	public static Repository getInstance() {
-		if(instance == null) {
-			instance = new Repository();
-			students = new HashSet<Student>();
+	public static Repository getInstance()  {
+		if (instance==null) {
+			instance= new Repository();
 		}
 		return instance;
 	}
 	
-	public boolean addStudent(Student student) {
-		return students.add(student);
+	public boolean ifExist(BeautyCompetition competition) {
+		return competitions.contains(competition);
 	}
 	
-	public Student getStudentByFN(String fn) {
-		for(Student s : students) {
-			if(s.getFacultyNumber().equals(fn)) {
-				return s;
-			}
-		}
-		return null;
+	public void addParticipant(BeautyCompetition competition) {
+		competitions.add(competition);
+		updateXMLFile(); 
 	}
+	
+
+	public static Set<BeautyCompetition> getParticipants() {
+		return competitions;
+	}
+
+
+	public static void setSParticipants(Set<BeautyCompetition> competitions) {
+		Repository.competitions = competitions;
+	}
+
+
+	public void updateXMLFile() {
+		XmlWorker worker = new XmlWorker();
+		try {
+			worker.writeToXmlFile(pathToXml, this);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
