3119294ca45f634c01d4769cc4cad00f7a712225
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-06-14 20:15:43 +0300

    Add files via upload

---
+++ example/User.java
@@ -0,0 +1,103 @@
+package models;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="user")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class User implements Serializable {
+	private String username,password,name;
+	private int rust,kg;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRust() {
+		return rust;
+	}
+	public void setRust(int rust) {
+		this.rust = rust;
+	}
+	public int getKg() {
+		return kg;
+	}
+	public void setKg(int kg) {
+		this.kg = kg;
+	}
+	
+	public User() {
+	}
+	
+	public User(String username, String password, String name, int rust, int kg) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.rust = rust;
+		this.kg = kg;
+	}
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (kg != other.kg)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (rust != other.rust)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+	
+}
